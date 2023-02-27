a38177cb272b82218904afa5019b41f9f42196b8
Charles Anderson <master.sparkle@gmail.com>
2023-02-27 13:13:19 -0800

    Expand client testing - check errors

--- spec/lib/olcc_web/client_spec.rb
+++ spec/lib/olcc_web/client_spec.rb
@@ -13,7 +13,15 @@ RSpec.describe OlccWeb::Client do
   it "calls welcome when we create a client" do
     stubs.get("/WelcomeController") { [200, {}, "hello"] }
 
-    client.inspect
+    client
+    stubs.verify_stubbed_calls
+  end
+
+  it "throws an Error when the WelcomeController is mad" do
+    stubs.get("/WelcomeController") { [500, {}, "fail"] }
+
+    expect { client }
+      .to raise_error("Failed to open WelcomeController - status: 500")
     stubs.verify_stubbed_calls
   end
 
@@ -21,7 +29,7 @@ RSpec.describe OlccWeb::Client do
     stubs.get("/WelcomeController") { [200, {}, "hello"] }
     stubs.get("/FrontController") { [200, {}, "all the rums"] }
 
-    puts client.select_category("RUM")
+    client.select_category("RUM")
     stubs.verify_stubbed_calls
   end
 
@@ -30,13 +38,21 @@ RSpec.describe OlccWeb::Client do
     inv_html = open_html_fixture("barcelo-detail.html")
     stubs.get("/FrontController") { [200, {}, inv_html] }
 
-    puts client.get_item_inventory("99900592775", "5927B")
-
-    # dallas_store_num = "1016"  # can't use store_num from fixture data - should defined this somewhere
-    # dallas_inventory = inv.find { |i| i[:store_num] == dallas_store_num }
-    # assert_not_nil dallas_inventory
-    # assert_equal 6, dallas_inventory[:qty]
+    inv = client.get_item_inventory("99900592775", "5927B")
+    dallas_store_num = "1016"  # can't use store_num from fixture data - should defined this somewhere
+    dallas_inventory = inv.find { |i| i[:store_num] == dallas_store_num }
+    expect(dallas_inventory).to_not be_nil
+    expect(dallas_inventory[:qty]).to eq(6)
 
     stubs.verify_stubbed_calls
+  end
+
+  it "throws an Error when there is a quiet error" do
+    stubs.get("/WelcomeController") { [200, {}, "hello"] }
+    err_html = open_html_fixture("quiet-error.html")
+    stubs.get("/FrontController") { [200, {}, err_html] }
+
+    expect { client.get_item_inventory("99900592775", "5927B") }
+      .to raise_error("OLCC error page encountered")
   end
 end
