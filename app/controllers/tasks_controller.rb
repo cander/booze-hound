f02db1ea7761d7ea6efa330f7c7da5d99c616442
Charles Anderson <master.sparkle@gmail.com>
2023-06-01 15:54:18 -0700

    Add real actions for daily tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,7 +8,21 @@ class TasksController < ApplicationController
   before_action :authenticate
 
   def daily
-    render plain: "OK\n\n"
+    response.headers["Content-Type"] = "text/event-stream"
+    if Rails.env.test?
+      response.stream.write "OK\n"
+      return
+    end
+
+    client = OlccWeb::Client.new(Rails.logger)
+    count = OlccBottle.count
+    UpdateCategoryBottles.call(client, "DOMESTIC WHISKEY")
+    response.stream.write "Found #{OlccBottle.count - count} new bottles\n"
+    count = OlccInventory.count
+    UpdateAllInventory.call(client)
+    response.stream.write "There are now #{OlccInventory.count - count} new inventory records\n"
+  ensure
+    response.stream.close
   end
 
   private
