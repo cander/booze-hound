88ec9e41534e236850fb8b35a55c84c17da3ac99
Kenny Carlile <kennycarlile@gmail.com>
2023-09-25 21:08:10 -0700

    Modified docker-dev/server-ctl.sh to handle existing db/development.sqlite3 file and to handle resetting modifications on db/schema.rb.

--- docker-dev/server-ctl.sh
+++ docker-dev/server-ctl.sh
@@ -17,19 +17,34 @@ case "$1" in
         ;;
     loaddb)
         prepare_task=""
-        echo "Load the development database with data from db/data.yml"
+        echo "Load the development database with data from db/data.yml."
         if [ -f db/development.sqlite3 ] ; then
-            echo "Backing up existing development DB to /tmp"
+            echo "Backing up existing development DB to /tmp/."
             cp db/development.sqlite3 /tmp
-        else
-            echo "There is no existing development DB. We will create one before loading."
-            prepare_task="db:prepare"
+            echo "Previous DB backed up to /tmp/."
+            echo ""
+
+            echo "Removing any remaining existing DB file."
+            rm db/development.sqlite3
+            echo ""
         fi
+        
+        echo "There is no existing development DB (or it has been removed)."
+        echo "Create a new one before loading."
+        prepare_task="db:prepare"
+        
         echo "Running $prepare_task db:data:load..."
         docker run -it --rm -v $(pwd):/rails $IMAGE_TAG bin/rake $prepare_task db:data:load
+
+        # check for file cleanup
+        if [[ `git status | grep "schema.rb"` ]]; then
+            echo "db/schema.rb needs to be reset."
+            git checkout HEAD -- db/schema.rb
+            echo "db/schema.rb has been reset."
+        fi
         ;;
     run)
-        echo "Running Rails app on port 3000. Use ctl-C to exit"
+        echo "Running Rails app on port 3000. Use ctl-C to exit."
         echo "Wait until you see the message, 'Listening on http://0.0.0.0:3000'"
         echo "Then you can go to:"
         echo ""
