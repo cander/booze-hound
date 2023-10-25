73398d16bb9ef5e26bd7bdb4fd16e112b0d8bf21
Charles Anderson <master.sparkle@gmail.com>
2023-10-25 15:09:09 -0700

    WIP - Controller for adding a bottle by code

--- app/controllers/olcc_bottles_controller.rb
+++ app/controllers/olcc_bottles_controller.rb
@@ -25,4 +25,32 @@ class OlccBottlesController < ApplicationController
 
     redirect_to @bottle
   end
+
+  def create
+    new_item_code = clean_item_code_param(:new_item_code)
+    category = params[:category]  # sanitize?
+    unless new_item_code && category
+      flash[:notice] = "An error occurred finding that bottle. Sorry."
+      redirect_back(fallback_location: root_path)
+      return
+    end
+
+    bottle = LoadBottle.call(olcc_client, category, new_item_code, "none")
+    if bottle
+      # success - show it
+      redirect_to bottle
+    else
+      flash[:notice] = "An error occurred finding bottle #{new_item_code}. Sorry."
+      redirect_back(fallback_location: root_path)
+    end
+  end
+
+  private
+
+  def clean_item_code_param(key)
+    val = "" && params[key].strip
+    if /^\d+$/.match?(val)
+      val
+    end
+  end
 end
