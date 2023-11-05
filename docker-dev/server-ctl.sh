ead9af91e7d55ec8f6e25bdf6d7c05277c9205ed
Kenny Carlile <kennycarlile@gmail.com>
2023-11-04 18:34:37 -0700

    Updating server-ctl.sh for modularity and rr cmd
    Updating server-ctl.sh for modularity and additional command rr.

--- docker-dev/server-ctl.sh
+++ docker-dev/server-ctl.sh
@@ -14,60 +14,89 @@ function docker_run() {
     docker run -it --rm -v $(pwd):/rails -p 3000:3000 $IMAGE_TAG $*
 }
 
+function docker_build() {
+    echo "Rebuild the Docker image..."
+    now=`date`
+    docker build -t $IMAGE_TAG -f docker-dev/Dockerfile --build-arg BUNDLE_DATE="$now" .
+}
+
+function loaddb() {
+    prepare_task=""
+    echo "Load the development database with data from db/data.yml."
+    if [ -f db/development.sqlite3 ] ; then
+        echo "Backing up existing development DB to /tmp/."
+        cp db/development.sqlite3 /tmp
+        echo "Previous DB backed up to /tmp/."
+        echo ""
+
+        echo "Removing any remaining existing DB file."
+        rm db/development.sqlite3
+        echo ""
+    fi
+
+    echo "There is no existing development DB (or it has been removed)."
+    echo "Create a new one before loading."
+    prepare_task="db:prepare"
+
+    echo "Running $prepare_task db:data:load..."
+    docker_run bin/rake $prepare_task db:data:load
+
+    # check for file cleanup
+    if [[ `git status | grep "schema.rb"` ]]; then
+        echo "db/schema.rb needs to be reset."
+        git checkout HEAD -- db/schema.rb
+        echo "db/schema.rb has been reset."
+    fi
+}
+
+function docker_run_continer() {
+    echo "Running Rails app on port 3000. Use ctl-C to exit."
+    echo "Wait until you see the message, 'Listening on http://0.0.0.0:3000'"
+    echo "Then you can go to:"
+    echo ""
+    echo "     http://localhost:3000"
+    docker_run
+}
+
+function migratedb() {
+    docker_run bin/rake db:migrate
+}
+
+function docker_console() {
+    docker_run /bin/bash
+}
+
+function default_option() {
+    echo "Usage: $0 [build|run|rr|console|loaddb|migratedb]"
+    echo "     build: Rebuild the Docker image with the latest dependencies"
+    echo "       run: Run the Rails app"
+    echo "        rr: Rebuild and run the Docker image with the latest dependencies"
+    echo "   console: Open a bash shell running in the Docker container"
+    echo "    loaddb: Load the database with initial data"
+    echo " migratedb: Run database migrations to update the schema"
+}
+
 case "$1" in
     build)
-        echo "Rebuild the Docker image..."
-        now=`date`
-        docker build -t $IMAGE_TAG -f docker-dev/Dockerfile --build-arg BUNDLE_DATE="$now" .
+        docker_build
         ;;
     console)
-        docker_run /bin/bash
+        docker_console
         ;;
     loaddb)
-        prepare_task=""
-        echo "Load the development database with data from db/data.yml."
-        if [ -f db/development.sqlite3 ] ; then
-            echo "Backing up existing development DB to /tmp/."
-            cp db/development.sqlite3 /tmp
-            echo "Previous DB backed up to /tmp/."
-            echo ""
-
-            echo "Removing any remaining existing DB file."
-            rm db/development.sqlite3
-            echo ""
-        fi
-        
-        echo "There is no existing development DB (or it has been removed)."
-        echo "Create a new one before loading."
-        prepare_task="db:prepare"
-        
-        echo "Running $prepare_task db:data:load..."
-        docker_run bin/rake $prepare_task db:data:load
-
-        # check for file cleanup
-        if [[ `git status | grep "schema.rb"` ]]; then
-            echo "db/schema.rb needs to be reset."
-            git checkout HEAD -- db/schema.rb
-            echo "db/schema.rb has been reset."
-        fi
+        loaddb
         ;;
     migratedb)
-        docker_run bin/rake db:migrate
+        migratedb
         ;;
     run)
-        echo "Running Rails app on port 3000. Use ctl-C to exit."
-        echo "Wait until you see the message, 'Listening on http://0.0.0.0:3000'"
-        echo "Then you can go to:"
-        echo ""
-        echo "     http://localhost:3000"
-        docker_run
+        docker_run_continer
+        ;;
+    rr)
+        docker_build
+        docker_run_continer
         ;;
     *)
-        echo "Usage: $0 [build|run]"
-        echo "     build: Rebuild the Docker image with the latest dependencies"
-        echo "   console: Open a bash shell running in the Docker container"
-        echo "    loaddb: Load the database with initial data"
-        echo " migratedb: Run database migrations to update the schema"
-        echo "       run: Run the Rails app"
+        default_option
         ;;
 esac
