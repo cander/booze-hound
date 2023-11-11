035c9432cb2500bd24b3831e34c32c86da3c6ffe
Kenny Carlile <kennycarlile@gmail.com>
2023-11-11 11:42:32 -0800

    Cleaning up js/main.js by removing auth/anon simulation code

--- public/js/main.js
+++ public/js/main.js
@@ -18,21 +18,4 @@ function getCopyrightRange(startYear = 2023) {
 $(document).ready(function() {
     let footerCopyrightRangeElement = $("#footerCopyrightRange");
     footerCopyrightRangeElement.text(getCopyrightRange(footerCopyrightRangeElement.text()));
-
-    // Setup auth/anon simulation
-    $('.authenticated-display').hide();
-    $('.anonymous-display').hide();
-    $('.authenticated-display').show(); // TODO: remove this line, used to force auth mode
-
-    $('input[type=radio][name=auth-options]').change(function() {
-        if ($('#anon-toggle').is(':checked')) {
-            $('.authenticated-display:not(.exclude)').hide();
-            $('.anonymous-display').show();
-        }
-        else if ($('#auth-toggle').is(':checked')) {
-            $('.authenticated-display').show();
-            $('.anonymous-display').hide();
-        }
-    });
 }); // end jQuery document-ready
-
