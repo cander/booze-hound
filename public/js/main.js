273aac1ce902bbd8553d657ad35b7d355e5c9470
Kenny Carlile <KCarlile@users.noreply.github.com>
2023-11-16 10:40:34 -0800

    Fixing layouts and nav for issue #125 (#130)
    * Fixing layouts and nav for issue #125
    * Adding anonymous icon to anonymous link and divider
    * Cleaning up js/main.js by removing auth/anon simulation code
    * Adding .vscode to .gitignore
    * Fixing User link and indentation, removing borders
    * Lots of fixes, lots of formatting.
    * Simplifying page title to Booze Hound
    * Reordering top nav to match My Links

--- public/js/main.js
+++ public/js/main.js
@@ -1,38 +1,21 @@
 function getCopyrightRange(startYear = 2023) {
-    const enDash = '–';
+  const enDash = '–';
 
-    let copyrightRange = '';
-    let currentYear = new Date().getFullYear();
+  let copyrightRange = '';
+  let currentYear = new Date().getFullYear();
 
-    if (currentYear == Number(startYear)) {
-        copyrightRange = startYear;
-    } // end if test
-    else {
-        copyrightRange = startYear + '' + enDash + '' + currentYear;
-        console.log(copyrightRange);
-    } // end else test
+  if (currentYear == Number(startYear)) {
+    copyrightRange = startYear;
+  } // end if test
+  else {
+    copyrightRange = startYear + '' + enDash + '' + currentYear;
+    console.log(copyrightRange);
+  } // end else test
 
-    return copyrightRange;
+  return copyrightRange;
 } // end getCopyrightRange function
 
 $(document).ready(function() {
-    let footerCopyrightRangeElement = $("#footerCopyrightRange");
-    footerCopyrightRangeElement.text(getCopyrightRange(footerCopyrightRangeElement.text()));
-
-    // Setup auth/anon simulation
-    $('.authenticated-display').hide();
-    $('.anonymous-display').hide();
-    $('.authenticated-display').show(); // TODO: remove this line, used to force auth mode
-
-    $('input[type=radio][name=auth-options]').change(function() {
-        if ($('#anon-toggle').is(':checked')) {
-            $('.authenticated-display:not(.exclude)').hide();
-            $('.anonymous-display').show();
-        }
-        else if ($('#auth-toggle').is(':checked')) {
-            $('.authenticated-display').show();
-            $('.anonymous-display').hide();
-        }
-    });
+  let footerCopyrightRangeElement = $("#footerCopyrightRange");
+  footerCopyrightRangeElement.text(getCopyrightRange(footerCopyrightRangeElement.text()));
 }); // end jQuery document-ready
-
