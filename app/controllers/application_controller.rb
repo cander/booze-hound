07258cd5d1f1a4ca564ee8a4bec50ec1cd649f66
Charles Anderson <master.sparkle@gmail.com>
2023-11-29 14:41:45 -0800

    Replace @user in stores controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,17 @@ class ApplicationController < ActionController::Base
   def olcc_client
     @olcc_client ||= OlccWeb::Client.new(Rails.logger)
   end
+
+  protected
+
+  def check_and_setup_user
+    # this is overriding @user set in initialize
+    if user_signed_in?
+      @user = current_user
+    else
+      @user = nil   # XXX - remove this eventually
+      flash[:error] = "You must be logged in to access this section"
+      redirect_to home_url
+    end
+  end
 end
--- app/controllers/olcc_stores_controller.rb
+++ app/controllers/olcc_stores_controller.rb
@@ -1,25 +1,27 @@
 class OlccStoresController < ApplicationController
+  before_action :check_and_setup_user
+
   def index
     if params[:query]
       @olcc_stores = OlccStore.search(params[:query])
       @query = params[:query]
     else
-      @olcc_stores = UserStoresQuery.call(@user)
+      @olcc_stores = UserStoresQuery.call(current_user)
     end
   end
 
   def show
     @store = OlccStore.find(params[:id])
     # TODO: sort the inventory by the name on the bottle, which isn't on inventory
-    @inventory = InventoryQuery.call([@store], @user.following_bottle_ids)
+    @inventory = InventoryQuery.call([@store], current_user.following_bottle_ids)
   end
 
   def update
     @store = OlccStore.find(params[:id])
     if params[:olcc_store][:follow] == "true"
-      @user.favor_store(@store)
+      current_user.favor_store(@store)
     else
-      @user.unfavor_store(@store)
+      current_user.unfavor_store(@store)
     end
 
     redirect_to @store
