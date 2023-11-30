63bb43ef5602d977daf1e85670ff969d465e2f32
Charles Anderson <master.sparkle@gmail.com>
2023-11-30 15:01:35 -0800

    Removed User.first from ApplicationController
    Which triggered a fix to the HomeController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,4 @@
 class ApplicationController < ActionController::Base
-  def initialize
-    super
-    # TODO - write real authentication and session code, maybe in a separate
-    # subclass - like SessionController. In the meantime, here's a User for
-    # all controllers to use.
-    @user = User.first
-  end
-
   def olcc_client
     @olcc_client ||= OlccWeb::Client.new(Rails.logger)
   end
@@ -18,7 +10,6 @@ class ApplicationController < ActionController::Base
     if user_signed_in?
       @user = current_user
     else
-      @user = nil   # XXX - remove this eventually
       flash[:error] = "You must be logged in to access this section"
       redirect_to home_url
     end
