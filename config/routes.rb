8be8c6b7cc564bf8a863f7243720d7e336b1a2af
Charles Anderson <master.sparkle@gmail.com>
2024-01-03 15:57:29 -0800

    Lint fixes
    That's what I get for editing in vim rather than VScode.

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,11 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   get "home/index", as: :home
-  devise_for :users, controllers: { registrations: 'registrations' }
+  devise_for :users, controllers: {registrations: "registrations"}
   as :user do
     # after editing the profile, stay on the edit profile page as user_root
-    get 'users/profile', :to => 'devise/registrations#edit', :as => :user_root
-end
+    get "users/profile", to: "devise/registrations#edit", as: :user_root
+  end
 
   # Defines the root path route ("/")
   root "home#index"
