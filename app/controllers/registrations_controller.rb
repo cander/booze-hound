36f07dcc643edf2d4d9c827236f389bc2b0971d8
Charles Anderson <master.sparkle@gmail.com>
2024-01-03 15:11:20 -0800

    Re-enable password editing
    The current password is only displayed if the "change password" control
    is hit. Then, all three passwords are required.
    ATM, the behavior in our regsitration controller is such that:
    + you can edit anything except the PW w/o providing the current PW
    + otherwise, you can edit the password and any attribute in one shot

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,7 +4,14 @@ class RegistrationsController < Devise::RegistrationsController
   protected
 
   def update_resource(resource, params)
-    # TODO: check if password is present
-    resource.update_without_password(params)
+    if params[:password].blank?
+      params.delete(:password)
+      params.delete(:password_confirmation) if params[:password_confirmation].blank?
+      resource.update_without_password(params)
+    else
+      # if the password is provided, allow changing any attribute
+      # this could be modified to remove attributes we don't want to change
+      super
+    end
   end
 end
